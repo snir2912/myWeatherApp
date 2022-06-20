71a88451548fb78b8b2b6153bbf93faa2db18fea
snir2912 <89028623+snir2912@users.noreply.github.com>
2022-06-20 20:02:37 +0300

    dad

--- server/src/utils/forecast.js
+++ server/src/utils/forecast.js
@@ -1,20 +1,20 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-        const url = 'http://api.weatherstack.com/current?access_key=38325228f56fe9dd59ad68463c71c701&query=' + latitude + ',' + longitude + '&inits=f'
+    const url = 'http://api.weatherstack.com/current?access_key=38325228f56fe9dd59ad68463c71c701&query=' + latitude + ',' + longitude + '&inits=f'
 
-        request({
-            url,
-            json: true
-        }, (error, response) => {
-            if (error) {
-                callback('unable to connect weather services', undefined)
-            } else if (response.body.error) {
-                callback('unable to fined your locattion', undefined)
-            } else {
-                callback(undefined, `<p>the sky today is: <strong>${response.body.current.weather_descriptions}</strong></p><p>the current temperasture is: <strong>${response.body.current.temperature}°</strong></p><p>its feels like: <strong>${response.body.current.feelslike}°</strong></p><p>chance of rain: <strong>${response.body.current.precip}%</strong></p>`)
-            }
-        })
-    }
-    // 'today is: ' + response.body.current.weather_descriptions + ',' + ' the current temperasture is: ' + response.body.current.temperature + '° and its feels like: ' + response.body.current.feelslike + '° and ' + response.body.current.precip + '% to rain')
+    request({
+        url,
+        json: true
+    }, (error, response) => {
+        if (error) {
+            callback('unable to connect weather services', undefined)
+        } else if (response.body.error) {
+            callback('unable to fined your locattion', undefined)
+        } else {
+            callback(undefined, 'today is: ' + response.body.current.weather_descriptions + ',' + ' the current temperasture is: ' + response.body.current.temperature + '° and its feels like: ' + response.body.current.feelslike + '° and ' + response.body.current.precip + '% to rain')
+        }
+    })
+}
+
 module.exports = forecast
